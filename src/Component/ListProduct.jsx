c4962984de912c80fb12e4f1a2772273093391fb
Ady Juliyanto <adyjuliyanto2001@gmail.com>
2021-02-10 00:25:14 +0700

    First Kasir App

---
+++ src/Component/ListProduct.jsx
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import {Col} from 'react-bootstrap';
+
+class ListProduct extends Component {
+    render(){
+        return(
+            <Col md={2} mt="4">
+                <h1>Pesanan Anda</h1>
+            </Col>
+        )
+    }
+}
+
+
+export default ListProduct;
